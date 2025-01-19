04dd78b70e953ec81af2d7083cbfc0f669091dc8
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-01-19 15:14:33 +0530

    🍞 go load env

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	godotenv.Load()
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "3000"
+	}
+	fmt.Println("PORT: ", PORT)
 }
