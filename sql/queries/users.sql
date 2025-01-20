4b56750a36f0461c5cb3b1e08a21418ddfe9de3f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-01-20 17:11:01 +0530

    🪪  unique feild api key configured

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -3,8 +3,13 @@ INSERT INTO users (
     id,
     created_at,
     updated_at,
-    name
+    name,
+    api_key
 ) VALUES (
-    $1, $2, $3, $4
+    $1, $2, $3, $4, encode(sha256(random()::text::bytea), 'hex')
 )
 RETURNING *;
+
+-- name: GetUserByAPIKey :one
+SELECT * FROM users
+WHERE api_key = $1;
