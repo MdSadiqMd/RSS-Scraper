a27bdf76158dcdf374408031e3c169ca3ef71357
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-01-21 20:57:31 +0530

    🪭 feed follow funcitons

---
+++ handler_feed_follow.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/MdSadiqMd/RSS-Scraper/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handleCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	type Parameters struct {
+		FeedID uuid.UUID `json:"feed_id"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := Parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, "Invalid request payload")
+	}
+
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      params.Name,
+		Url:       params.URL,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, "Unable to create feed: "+err.Error())
+		return
+	}
+
+	respondWithJSON(w, 201, databaseFeedToFeed(feed))
+}
+
+func (apiCfg *apiConfig) handleGetFeeds(w http.ResponseWriter, r *http.Request) {
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
+	if err != nil {
+		respondWithError(w, 400, "Unable to get feeds: "+err.Error())
+		return
+	}
+
+	respondWithJSON(w, 200, databaseFeedsToFeeds(feeds))
+}
