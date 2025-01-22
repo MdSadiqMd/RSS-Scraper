e9f0fddb704ecf4feb03c78b340500afcb4aaf85
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-01-22 12:09:49 +0530

    🐕 scrapper init

--- scrapper.go
+++ scrapper.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/MdSadiqMd/RSS-Scraper/internal/database"
@@ -12,13 +14,45 @@ func startScrapping(
 	concurrency int,
 	timeBetweenRequest time.Duration,
 ) {
-	log.Printf("Scrapping on %v goroutines every %v", concurrency, timeBetweenRequest)
-	ticker:=time.NewTicker(timeBetweenRequest)
+	log.Printf("🪛 Scrapping on %v goroutines every %v", concurrency, timeBetweenRequest)
+	ticker := time.NewTicker(timeBetweenRequest)
 	for ; ; <-ticker.C {
-		for i := 0; i < concurrency; i++ {
-			go func() {
-				db.Scrapping()
-			}()
+		feeds, err := db.GetNextFeedsToFetch(
+			context.Background(),
+			int32(concurrency),
+		)
+		if err != nil {
+			log.Println("⚠️ Unable to fetch feeds: ", err)
+			continue
 		}
+
+		wg := sync.WaitGroup{}
+		for _, feed := range feeds {
+			wg.Add(1)
+			go func(
+				db *database.Queries,
+				wg *sync.WaitGroup,
+				feed database.Feed,
+			) {
+				defer wg.Done()
+				_, err := db.MarkFeedAsFetched(context.Background(), feed.ID)
+				if err != nil {
+					log.Println("💀 Unable to mark feed as fetched: ", err)
+					return
+				}
+
+				rssFeed, err := urlToFeedURL(feed.Url)
+				if err != nil {
+					log.Println("💀 Unable to fetch feed: ", err)
+					return
+				}
+
+				for _, item := range rssFeed.Channel.Items {
+					log.Println("📰 Adding item: ", item.Title)
+				}
+				log.Println("✅ Successfully scrapped feed of name: %v, which consists of %v posts: ", feed.Name, len(rssFeed.Channel.Items))
+			}(db, &wg, feed)
+		}
+		wg.Wait()
 	}
 }
